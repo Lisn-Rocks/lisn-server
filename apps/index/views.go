17948aa597b761be9c0ac8b38fb843821c4e6b17
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2020-01-22 19:41:28 +0000

    Basic templating for the index page

---
+++ apps/index/views.go
@@ -0,0 +1,76 @@
+package index
+
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"database/sql"
+	"path"
+	"html/template"
+
+	"github.com/sharpvik/lisn-backend/config"
+)
+
+
+// TemplateData is a struct used for template generation.
+type TemplateData struct {
+	CssData			template.HTML
+	BodyContents	template.HTML
+	JavaScript		template.HTML
+}
+
+
+// MainHTMLTemplate is a string read from /templates/base.html -- it is the most
+// basic HTML template used in the project.
+var CSSData, _ = ioutil.ReadFile(
+	path.Join(config.AppsFolder, "index", "static", "style.html"),
+)
+
+
+// MainHTMLTemplate is a string read from /templates/base.html -- it is the most
+// basic HTML template used in the project.
+var MainHTMLTemplate, _ = ioutil.ReadFile(
+	path.Join(config.TemplatesFolder, "base.html"),
+)
+
+
+// Serve function is used to serve the main index of songs to the user.
+func Serve(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	data := TemplateData{
+		template.HTML( string(CSSData) ),
+		template.HTML( FormBodyContents(db) ),
+		template.HTML(""),
+	}
+
+	tmpl, err := template.New("index").Parse( string(MainHTMLTemplate) )
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+
+// FormBodyContents returns an HTML list of songs we have in the database.
+func FormBodyContents(db *sql.DB) (cont string) {
+	rows, _ := db.Query("SELECT title, author FROM songs")
+
+	var title, author string
+
+	cont += "<ol>"
+
+	for rows.Next() {
+		rows.Scan(&title, &author)
+		cont += fmt.Sprintf("<li><i>%s</i> by %s</li>", title, author)
+	}
+
+	cont += "</ol>"
+	return
+}
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/sharpvik/lisn-backend/config"
+	"github.com/sharpvik/lisn-backend/apps/index"
 )
 
 
@@ -19,52 +20,6 @@ var logr *log.Logger
 var mux *http.ServeMux
 
 var db *sql.DB
-
-
-
-type mainHandler struct {}
-
-// ServeHTTP function is the entry point for server's routing mechanisms.
-// It uses mux to delegate request to a proper handler function.
-func (*mainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	logr.Printf( "URL: %s", r.URL.String() )
-	mux.ServeHTTP(w, r)
-}
-
-
-
-func serveRandomSong(w http.ResponseWriter, r *http.Request) {
-	rows, _ := db.Query("SELECT id FROM songs")
-
-	var id int
-
-	rows.Next()
-	rows.Scan(&id)
-
-	path := fmt.Sprintf("store/%d.mp3", id)
-	http.ServeFile(w, r, path)
-}
-
-func serveSongByID(w http.ResponseWriter, r *http.Request) {
-	id := "1" // in production id must be read from request body (type string)
-	path := fmt.Sprintf("store/%s.mp3", id)
-	http.ServeFile(w, r, path)
-}
-
-
-
-func initDB(db *sql.DB) {
-	stmt, _ := db.Prepare("CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, title TEXT, duration FLOAT, genre TEXT, author TEXT, album TEXT NULL)")
-	stmt.Exec()
-}
-
-func insertSongs(db *sql.DB) {
-	stmt, _ := db.Prepare("INSERT INTO songs (title, duration, genre, author, album) VALUES (?, ?, ?, ?, ?)")
-	stmt.Exec("Another One Bites the Dust", 3.7, "Classic Rock", "Queen", "The Game")
-	stmt.Exec("Don't Stop Me Now", 3.617, "Classic Rock", "Queen", "Jazz")
-	stmt.Exec("I Want To Break Free", 4.517, "Classic Rock", "Queen", "The Works")
-	stmt.Exec("Somebody To Love", 5.15, "Rock", "Queen", "A Day at the Races")
-}
 
 
 
@@ -88,10 +43,50 @@ func main() {
 
 
 	mux = http.NewServeMux()
-	mux.HandleFunc("/", serveRandomSong)
 	mux.HandleFunc("/id", serveSongByID)
 
 
-	logr.Printf("Serving at localhost:%s", config.Port)
+	logr.Printf("Serving at localhost%s", config.Port)
 	server.ListenAndServe()
+}
+
+
+
+type mainHandler struct {}
+
+// ServeHTTP function is the entry point for server's routing mechanisms.
+// It uses mux to delegate request to a proper handler function.
+func (*mainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	logr.Printf( "URL: %s", r.URL.String() )
+
+	switch r.URL.String() {
+	case "/":
+		index.Serve(w, r, db)
+
+	default:
+		mux.ServeHTTP(w, r)
+	}
+}
+
+
+
+func serveSongByID(w http.ResponseWriter, r *http.Request) {
+	id := "1" // in production id must be read from request body (type string)
+	path := fmt.Sprintf("store/%s.mp3", id)
+	http.ServeFile(w, r, path)
+}
+
+
+
+func initDB(db *sql.DB) {
+	stmt, _ := db.Prepare("CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, title TEXT, duration FLOAT, genre TEXT, author TEXT, album TEXT NULL)")
+	stmt.Exec()
+}
+
+func insertSongs(db *sql.DB) {
+	stmt, _ := db.Prepare("INSERT INTO songs (title, duration, genre, author, album) VALUES (?, ?, ?, ?, ?)")
+	stmt.Exec("Another One Bites the Dust", 3.7, "Classic Rock", "Queen", "The Game")
+	stmt.Exec("Don't Stop Me Now", 3.617, "Classic Rock", "Queen", "Jazz")
+	stmt.Exec("I Want To Break Free", 4.517, "Classic Rock", "Queen", "The Works")
+	stmt.Exec("Somebody To Love", 5.15, "Rock", "Queen", "A Day at the Races")
 }
